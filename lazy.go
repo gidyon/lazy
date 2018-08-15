8b92e7f06b47ca2949412c403b0e00803f484ce2
gidyon <gideon.ngk@gmail.com>
2018-08-15 13:51:09 +0300

    created a type to absract some stuff, added resource freeing, there is more work to be done!

--- lazy.go
+++ lazy.go
@@ -1,47 +1,59 @@
-// Package lazy lazily generates values in a given sequence
 package lazy
 
-// Any is a type that will later be unexported
-type Any interface{}
+// import "time"
 
-// EvalFunc is another one
-type EvalFunc func(Any) (Any, Any)
+// // Any is a type that will later be unexported
+// type Any interface{}
 
-// BuildLazyEvaluator is a higher-order function that lazily evaluates next value in a sequence
-func buildLazyEvaluator(eval EvalFunc, initState Any) func(...int) Any {
-	retValChan := make(chan Any)
-	quitChan := make(chan struct{})
-	retValFunc := func(sig ...int) Any {
-		if len(sig) > 0 && sig[0] < 0 {
-			defer func() { quitChan <- struct{}{} }()
-		}
-		return <-retValChan
-	}
-	go func() {
-		var actState = initState
-		var retVal Any
-		for {
-			retVal, actState = eval(actState)
-			select {
-			case <-quitChan:
-				close(quitChan)
-				close(retValChan)
-				return
-			case retValChan <- retVal:
-			}
-		}
-	}()
-	return retValFunc
-}
+// // EvalFunc is another one
+// type EvalFunc func(Any) (Any, Any)
 
-// Ints is a wrapper to BuildLazyEvaluator that lazily generate integer values in sequence
-func Ints(eval EvalFunc, initState Any) func(...int) int {
-	ef := buildLazyEvaluator(eval, initState)
-	return func(sig ...int) int {
-		val := ef(sig...)
-		if val == nil {
-			return 0
-		}
-		return val.(int)
-	}
-}
+// // BuildLazyEvaluator is a higher-order function that lazily evaluates next value in a sequence
+// func buildLazyEvaluator(eval EvalFunc, initState Any) func(...int) Any {
+// 	retValChan := make(chan Any)
+// 	quitChan := make(chan struct{})
+// 	retValFunc := func(sig ...int) Any {
+// 		if len(sig) > 0 && sig[0] < 0 {
+// 			defer func() { quitChan <- struct{}{} }()
+// 		}
+// 		return <-retValChan
+// 	}
+// 	go func() {
+// 		var actState = initState
+// 		var retVal Any
+// 		for {
+// 			retVal, actState = eval(actState)
+// 			select {
+// 			case <-quitChan:
+// 				close(quitChan)
+// 				close(retValChan)
+// 				return
+// 			case retValChan <- retVal:
+// 			}
+// 		}
+// 	}()
+// 	return retValFunc
+// }
+
+// // Ints is a wrapper to BuildLazyEvaluator that lazily generate integer values in sequence
+// func Ints(eval EvalFunc, initState Any) func(...int) int {
+// 	ef := buildLazyEvaluator(eval, initState)
+// 	return func(sig ...int) int {
+// 		val := ef(sig...)
+// 		if val == nil {
+// 			return 0
+// 		}
+// 		return val.(int)
+// 	}
+// }
+
+// func IntsWithTimeout(eval EvalFunc, initState, timeout time.Duration) func() int {
+// 	ef := buildLazyEvaluator(eval, initState)
+// 	return func() int {
+// 		val := ef()
+// 		if val == nil {
+// 			return 0
+// 		}
+// 		return val.(int)
+// 	}
+// }
