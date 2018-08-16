dfabe6e972d5630160e1c3f7ded145c443ea652d
gidyon <gideon.ngk@gmail.com>
2018-08-16 10:27:24 +0300

    added timeout and limit to the generator

--- lazy.go
+++ lazy.go
@@ -1,59 +1,117 @@
+// Package lazy provides a generic utility for generating the next value in a sequence in a lazily manner
 package lazy
 
-// import "time"
+import "time"
 
-// // Any is a type that will later be unexported
-// type Any interface{}
+// Any is a type for the empty interface and all types will satisfy
+type Any interface{}
 
-// // EvalFunc is another one
-// type EvalFunc func(Any) (Any, Any)
+// EvalFunc is a function type used to generate the values in a sequence.
+type EvalFunc func(Any) (Any, Any)
 
-// // BuildLazyEvaluator is a higher-order function that lazily evaluates next value in a sequence
-// func buildLazyEvaluator(eval EvalFunc, initState Any) func(...int) Any {
-// 	retValChan := make(chan Any)
-// 	quitChan := make(chan struct{})
-// 	retValFunc := func(sig ...int) Any {
-// 		if len(sig) > 0 && sig[0] < 0 {
-// 			defer func() { quitChan <- struct{}{} }()
-// 		}
-// 		return <-retValChan
-// 	}
-// 	go func() {
-// 		var actState = initState
-// 		var retVal Any
-// 		for {
-// 			retVal, actState = eval(actState)
-// 			select {
-// 			case <-quitChan:
-// 				close(quitChan)
-// 				close(retValChan)
-// 				return
-// 			case retValChan <- retVal:
-// 			}
-// 		}
-// 	}()
-// 	return retValFunc
-// }
+// Options provide a way for passing options to use for the Generator
+type Options struct {
+	Generator EvalFunc
+	Limit     uint
+	Timeout   time.Duration
+	InitState Any
+}
 
-// // Ints is a wrapper to BuildLazyEvaluator that lazily generate integer values in sequence
-// func Ints(eval EvalFunc, initState Any) func(...int) int {
-// 	ef := buildLazyEvaluator(eval, initState)
-// 	return func(sig ...int) int {
-// 		val := ef(sig...)
-// 		if val == nil {
-// 			return 0
-// 		}
-// 		return val.(int)
-// 	}
-// }
+type sequenceGenerator struct {
+	quit      chan struct{}
+	generator EvalFunc
+	limit     uint
+	timeout   time.Duration
+	init      Any
+}
 
-// func IntsWithTimeout(eval EvalFunc, initState, timeout time.Duration) func() int {
-// 	ef := buildLazyEvaluator(eval, initState)
-// 	return func() int {
-// 		val := ef()
-// 		if val == nil {
-// 			return 0
-// 		}
-// 		return val.(int)
-// 	}
-// }
+// NewLazyGenerator returns a generator object
+func NewLazyGenerator(gen EvalFunc, opt ...Options) *sequenceGenerator {
+	var (
+		timeout      = time.Duration(time.Second * 60 * 60 * 24)
+		limit   uint = 1000000
+		init    Any
+	)
+	if len(opt) != 0 {
+		if opt[0].Timeout == 0 {
+			timeout = time.Second * 60 * 60 * 24
+		} else {
+			timeout = opt[0].Timeout
+		}
+		if opt[0].Limit == 0 {
+			limit = 10000000
+		} else {
+			limit = opt[0].Limit
+		}
+	}
+	return &sequenceGenerator{
+		quit:      make(chan struct{}),
+		generator: gen,
+		limit:     limit,
+		timeout:   timeout,
+		init:      init,
+	}
+}
+
+// SetTimeout sets the duration of the generator. When not set, default is one year
+func (sq *sequenceGenerator) SetTimeout(d time.Duration) *sequenceGenerator {
+	sq.timeout = d
+	return sq
+}
+
+// SetInit sets the initial values for the generator
+func (sq *sequenceGenerator) SetInit(initState Any) *sequenceGenerator {
+	sq.init = initState
+	return sq
+}
+
+// SetLimit sets the number of times the generator will generate values. When not set, default is 10000000
+func (sq *sequenceGenerator) SetLimit(n uint) *sequenceGenerator {
+	sq.limit = n
+	return sq
+}
+
+// Stop sends a signal to generator to free resources
+func (sq *sequenceGenerator) Stop() {
+	sq.quit <- struct{}{}
+}
+
+// Limit returns the number of times the generator will run.
+func (sq *sequenceGenerator) Limit() int {
+	return int(sq.limit)
+}
+
+func (sq *sequenceGenerator) cleanup(ch chan Any) {
+	close(ch)
+	close(sq.quit)
+}
+
+// Generate returns a generator function that generates the next value in the sequence
+func (sq *sequenceGenerator) Generate() func() Any {
+	return sq.buildLazyGenerator()
+}
+
+func (sq *sequenceGenerator) buildLazyGenerator() func() Any {
+	retValChan := make(chan Any)
+	go func() {
+		var actState = sq.init
+		var retVal Any
+		timeout := time.After(sq.timeout)
+		for i := 0; i < int(sq.limit); i++ {
+			retVal, actState = sq.generator(actState)
+			select {
+			case <-sq.quit:
+				sq.cleanup(retValChan)
+				return
+			case retValChan <- retVal:
+			case <-timeout:
+				sq.cleanup(retValChan)
+				return
+			}
+		}
+		sq.cleanup(retValChan)
+	}()
+	return func() Any {
+		return <-retValChan
+	}
+}
